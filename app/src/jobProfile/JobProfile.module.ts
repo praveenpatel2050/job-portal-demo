bf6a4c709c9f1218c0dc8cbd3e055d077eb61369
Praveen Patel <praveen.p.patel@shell.com>
2025-02-18 10:34:58 +0530

    Refactor job profile module and controller; update database connection and service methods for improved functionality

--- app/src/jobProfile/JobProfile.module.ts
+++ app/src/jobProfile/JobProfile.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose';
-import { JobProfileModel } from '../model/JobProfile.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { connectionString } from 'src/config/db.config';
+import { JobProfileSchema } from '../model/JobProfile.model';
 import { JobProfileService } from './JobProfile.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'JobProfile', schema: JobProfileModel}])],
-    providers: [JobProfileService],
-    exports: [JobProfileService],
+  imports: [
+    MongooseModule.forRoot(connectionString),
+    MongooseModule.forFeature([
+      { name: 'JobProfile', schema: JobProfileSchema },
+    ]),
+  ],
+  providers: [JobProfileService],
+  exports: [JobProfileService],
 })
 export class JobProfileModule {}
